762b009464d5730f65793ba2c76480d4debaccb5
nfountain <37487498+nfountain@users.noreply.github.com>
2018-05-13 14:58:21 -0600

    Upload files - initial commit
    Need to work on the slide open menu for mobile.

---
+++ RWD_Brighton_Times/interactivity.js
@@ -0,0 +1,9 @@
+// Trying to say button... onclick... toggleclass to nav.open and then toggleClass off of nav.open. How to do this???
+var openMenu;
+openMenu = $('nav').toggleClass('.open');
+
+$('#nav_menu')on.('click', function() {
+openMenu;
+});
+
+// also need to add an onkeypress for accessibility/tabbing
